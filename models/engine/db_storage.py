2a3b7fc2dfaeb054f31cbee7b40bc803bc363603
Air-KS <kevinrogeret@gmail.com>
2023-08-15 16:57:19 +0200

    Add def close method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -65,3 +65,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session_factory)
+
+    def close(self):
+        """Remove private session attribute"""
+        self.__session.close()
