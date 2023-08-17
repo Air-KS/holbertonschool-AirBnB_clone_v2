7e6adb67a1c60bc0a9ac702d0d395f7763ea805c
Air-KS <kevinrogeret@gmail.com>
2023-08-17 09:08:45 +0000

    Inverse teardown by /states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,16 +5,11 @@ Host = 0.0.0.0, port = 5000
 Routes:
     /: Displays 'Hello HBNB!'
 """
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 from models import storage
-from models.state import State
+
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def teardown_data(self):
-    """refresh data"""
-    storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
@@ -22,6 +17,12 @@ def states_list():
     """return all states in the db"""
     states = storage.all(State)
     return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_data(self):
+    """refresh data"""
+    storage.close()
 
 
 if __name__ == "__main__":
